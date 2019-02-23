b9832613ba55cfc3d1a6114835f54a687062cb1e
Arnaud Buathier <arnaud@arnapou.net>
2019-02-23 23:34:20 +0100

    refactor bootstrap to be safer and cleaner

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,15 @@
 <?php
 
-foreach (['count', 'sizeof'] as $target) {
+\call_user_func(function () {
+    foreach (['count', 'sizeof'] as $target) {
 
-    if (!is_file(__DIR__ . "/generated.php72fix.$target.php")) {
-        touch(__DIR__ . "/generated.php72fix.$target.php");
-    }
+        if (!is_file(__DIR__ . "/generated.php72fix.$target.php")) {
+            if (!touch(__DIR__ . "/generated.php72fix.$target.php")) {
+                trigger_error("File generated.php72fix.$target.php was not found and could not be created.", E_USER_WARNING);
+            }
+        }
 
-    if (!is_file(__DIR__ . "/generated.php72fix.$target.php")) {
-        trigger_error("File generated.php72fix.$target.php was not found.", E_USER_WARNING);
-    } else {
         include __DIR__ . "/generated.php72fix.$target.php";
-    }
 
-}
+    }
+});
